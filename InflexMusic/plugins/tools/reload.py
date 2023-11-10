495a7fdb70df9fbe9d746df034ef45290302d10f
Venom <141371272+NITISHSHARMA143123@users.noreply.github.com>
2023-11-10 20:32:06 +0530

    Update reload.py

--- InflexMusic/plugins/tools/reload.py
+++ InflexMusic/plugins/tools/reload.py
@@ -92,7 +92,7 @@ async def close_menu(_, CallbackQuery):
         await CallbackQuery.answer()
         await CallbackQuery.message.delete()
         await CallbackQuery.message.reply_text(
-            f"➜ 𝖢𝗅𝗈𝗌𝖾𝖽 𝖡𝗒 : {CallbackQuery.from_user.mention}"
+            f"𝖢𝗅𝗈𝗌𝖾𝖽 𝖡𝗒 ~> : {CallbackQuery.from_user.mention}"
         )
     except:
         pass
