b3d4a839851e05c419e9f20695845e4ee69cc921
Venom <141371272+NITISHSHARMA143123@users.noreply.github.com>
2023-11-21 21:55:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -36,7 +36,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/TeamInflex/InflexMusicBot",
+    "https://github.com/NITISHSHARMA143123/Cocktail",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
