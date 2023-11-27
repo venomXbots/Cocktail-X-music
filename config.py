f668b91d06c2ef484f6235d76df81eb21b9c97e1
Venom <141371272+NITISHSHARMA143123@users.noreply.github.com>
2023-11-27 21:19:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -36,9 +36,9 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/TeamInflex/InflexMusicBot",
+    "https://github.com/NITISHSHARMA143123/Cocktail",
 )
-UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
+UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
