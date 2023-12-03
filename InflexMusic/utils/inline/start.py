e897c6c2228dfcbeac66d2d9a02b6f4f2f2990bf
venomXbots <152503093+venomXbots@users.noreply.github.com>
2023-12-04 03:26:20 +0530

    Update start.py

--- InflexMusic/utils/inline/start.py
+++ InflexMusic/utils/inline/start.py
@@ -31,7 +31,7 @@ def private_panel(_):
         ],
         [
             InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID), 
-       
+            InlineKeyboardButton(text=_["S_B_5"], user_id=config.("https://t.me/Lily_support_chat"),
         ],
     ]
     return buttons
